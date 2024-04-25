93884455522c0511b7e8e5f20bc92a35834a1072
Lucas-Vergara <lucasvergara1@gmail.com>
2024-04-24 21:11:45 -0400

    Update header.html, header.css, v-down.svg, and header.js files with new dropdown menu functionality

--- assets/js/header.js
+++ assets/js/header.js
@@ -9,3 +9,30 @@ document.addEventListener('DOMContentLoaded', function () {
     navMenu.classList.toggle('active');
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach(function (dropdown) {
+    dropdown.querySelector('.dropbtn').addEventListener('click', function (event) {
+      event.preventDefault(); // Evita que el navegador siga el href
+      var currentDropdown = dropdown.querySelector('.dropdown-content');
+      // Cierra todos los dropdowns que estén abiertos
+      document.querySelectorAll('.dropdown-content').forEach(function (drop) {
+        if (drop !== currentDropdown) {
+          drop.style.display = 'none';
+        }
+      });
+      // Alterna el estado visible del dropdown actual
+      currentDropdown.style.display = currentDropdown.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+
+  // Cierra el dropdown si se hace clic en cualquier lugar fuera de él
+  window.addEventListener('click', function (event) {
+    if (!event.target.matches('.dropbtn')) {
+      document.querySelectorAll('.dropdown-content').forEach(function (dropdown) {
+        dropdown.style.display = 'none';
+      });
+    }
+  });
+});
